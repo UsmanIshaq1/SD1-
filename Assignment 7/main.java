c4759ff517bd5e26f5fd7093bb866b055360515c
UsmanIshaq1 <usmanishaq134@gmail.com>
2025-11-01 12:43:33 -0400

    Implement main method for enemy and wizard examples
    Added main method to demonstrate enemy and wizard functionality.

--- Assignment 7/main.java
+++ Assignment 7/main.java
@@ -3,3 +3,24 @@
 // Software Development 1
 // Professor Melissa
 
+public class Main {
+    public static void main(String[] args) {
+
+        // Generic enemy
+        Enemy enemy1 = new Enemy(100, 10);
+        enemy1.attack(); //calling the attack method from enemy
+
+        // Fire wizard
+        Wizard fire_wizard = new Wizard(80, 15, "fire");
+        fire_wizard.damageType();
+
+        // Create an ice wizard
+        Wizard iceWizard = new Wizard(90, 12, "ice");
+        iceWizard.damageType();
+
+        // Create a goblin
+        Goblin goblin = new Goblin(70, 8);
+        goblin.attack();
+    }
+}
+
