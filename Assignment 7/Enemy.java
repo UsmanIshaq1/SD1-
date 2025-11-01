bf6bcd0c564a6a713a0c2f0408144cdd14d84006
UsmanIshaq1 <usmanishaq134@gmail.com>
2025-11-01 12:44:32 -0400

    Add Enemy class with health and damage attributes

---
+++ Assignment 7/Enemy.java
@@ -0,0 +1,18 @@
+// Usman Ishaq
+// Assignment 7
+// Software Development 1
+// Professor Melissa
+
+public class Enemy {
+    int health;
+    int damage;
+
+    public Enemy(int h, int d){
+        health = h;
+        damage = d;
+    }
+
+    void attack(){
+        System.out.println("the enemy attacks");
+    }
+}
